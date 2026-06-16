3911b3c4e81d04f42d4780c48303ede67b77bb5f
GIANNI P. <jac.paoletti@gmail.com>
2026-06-16 07:38:59 +0200

    Update cache list in service worker
    Added icons to the cache list for the service worker.

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,10 @@
 const CACHE_NAME = 'pack-essentiel-v1';
 const urlsToCache = [
-  '.',
-  'index.html',
-  'manifest.json'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
